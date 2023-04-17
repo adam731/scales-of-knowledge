e3df25766632a45a6e0ea0a08aa56d65b3ab725c
brady <bradysmithcoding@gmail.com>
2023-04-17 02:52:42 -0230

    got majority of functionality done

--- frontend/src/components/Leaderboard.js
+++ frontend/src/components/Leaderboard.js
@@ -1,10 +1,13 @@
-import React, { useEffect } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { leaderboardFetch } from "../apiCalls/fetch.js";
 import "../css/leaderboard.css";
 import Header from "./Header.js";
 import Logout from "./Logout.js";
 
 function Leaderboard() {
+  const [leaderboardData, setLeaderboardData] = useState([]);
+  const [userTotalScore, setUserTotalScore] = useState(0);
   const navigate = useNavigate();
   const user = JSON.parse(localStorage.getItem("user"));
   useEffect(() => {
@@ -12,15 +15,50 @@ function Leaderboard() {
       // Redirect to the login page if the user is not logged in
       navigate("/");
     }
-  });
+
+    async function fetchData() {
+      const data = await leaderboardFetch(user.username);
+      setUserTotalScore(data.userStats);
+      setLeaderboardData(data.globalStats);
+    }
+    fetchData();
+  }, []);
+  console.log(leaderboardData.globalStats);
 
   return (
     <div className="leaderboardPage">
-      <h1>{user && user.username}</h1>
       <Header />
       <Logout />
       <div className="leaderboard">
-        <Link to="/dashboard" className="backButton">
+        {leaderboardData.length > 0 && (
+          <div className="leaderTable">
+            <table className="">
+              <tr>
+                <th>Rank</th>
+                <th>User</th>
+                <th>Total Score</th>
+              </tr>
+              {leaderboardData.map((user, index) => (
+                <tr>
+                  <td>{index + 1}.</td>
+                  <td>{user.username}</td>
+                  <td>{user.score}</td>
+                </tr>
+              ))}
+            </table>
+          </div>
+        )}
+        <div>
+          <h1>Your total score: {userTotalScore}</h1>
+        </div>
+        <Link
+          to="/dashboard"
+          style={{
+            color: "#3A98B9",
+            textDecoration: "none",
+            fontSize: "1.5rem",
+          }}
+        >
           Back
         </Link>
       </div>
--- frontend/src/components/Trivia.js
+++ frontend/src/components/Trivia.js
@@ -1,37 +1,87 @@
 import React, { useEffect, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import logo from "../images/logo.png";
 import "../css/trivia.css";
 import Header from "./Header.js";
-import Logout from "./Logout.js";
-import { questionsFetch } from "../apiCalls/fetch.js";
+import { leaderboardPatch, questionsFetch } from "../apiCalls/fetch.js";
 
 function Trivia() {
   const navigate = useNavigate();
   const user = JSON.parse(localStorage.getItem("user"));
   const [questionsData, setQuestionsData] = useState([]);
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const [score, setScore] = useState(0);
+
+  const handleSelect = async (answer) => {
+    if (answer === questionsData[questionIndex].correctAnswer) {
+      setScore(score + 20);
+      setCorrect(correct + 1);
+    }
+    if (questionIndex === 4) {
+      setQuestionIndex(questionIndex + 1);
+    } else {
+      setQuestionIndex(questionIndex + 1);
+    }
+  };
 
   useEffect(() => {
     if (!user) {
       // Redirect to the login page if the user is not logged in
       navigate("/");
     }
+
+    if (questionIndex === 5) {
+      leaderboardPatch(user.username, { score: score });
+    }
     async function fetchData() {
       const data = await questionsFetch();
       setQuestionsData(data[0].questions);
     }
     fetchData();
-  }, []);
+  }, [score, user.username, questionIndex]);
   return (
     <div className="triviaPage">
-      <div></div>
-      <h1>{user && user.username}</h1>
       <Header />
-      <Logout />
       <div className="trivia">
-        <Link to="/dashboard" className="backButton">
-          Back
-        </Link>
+        <div className="questionaire">
+          {questionIndex === 5 ? (
+            <h1>See you tomorrow!</h1>
+          ) : (
+            questionsData.length > 0 && (
+              <div className="questionTitle">
+                <h1>{questionsData[questionIndex].question}</h1>
+              </div>
+            )
+          )}
+          {questionIndex === 5 ? (
+            <div className="finished">
+              <h1>You got {correct} / 5 correct!</h1>
+              <h1>Your score is {score}</h1>
+              <Link
+                to="/dashboard"
+                style={{
+                  color: "#3A98B9",
+                  textDecoration: "none",
+                  fontSize: "1.5rem",
+                }}
+              >
+                Back
+              </Link>
+            </div>
+          ) : (
+            questionsData.length > 0 && (
+              <div className="question">
+                {questionsData[questionIndex].options.map((item, index) => (
+                  <div className="buttons">
+                    <button onClick={() => handleSelect(item, index)}>
+                      {item}
+                    </button>
+                  </div>
+                ))}
+              </div>
+            )
+          )}
+        </div>
       </div>
     </div>
   );
